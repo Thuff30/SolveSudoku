22bc55127a272d72588759b1106800fe8da8f240
Thuff30 <24842812+Thuff30@users.noreply.github.com>
2020-07-25 00:26:36 -0400

    Update SolveSudoku.py

--- SolveSudoku.py
+++ SolveSudoku.py
@@ -1,42 +1,64 @@
 import numpy as np
 
-puzzle = [[3,7,0,0,5,0,2,0,0],\
-  [4,0,0,7,0,6,0,1,0],\
-  [0,0,1,0,0,0,0,0,0],\
-  [0,0,0,1,7,0,6,4,0],\
-  [0,0,0,0,0,9,0,0,0],\
-  [0,9,0,0,0,0,3,0,5],\
-  [0,0,0,0,2,0,0,0,0],\
-  [0,5,7,8,0,0,0,0,4],\
-  [0,0,0,0,0,0,0,8,0]]
+def input_to_list(input):
+  listin = input.split(",")
+  listout = list(map(int, listin))
+  return listout
 
 def reviewpossibles(y,x,n):
   global puzzle
+  #check column
   for i in range(0,9):
     if puzzle[y][i] == n:
       return False
+  #check row
   for i in range(0,9):
     if puzzle[i][x] == n:
       return False
-  x0 = (x//3)*3
-  y0 = (y//3)*3
+  x2 = (x//3)*3
+  y2 = (y//3)*3
+  #check square
   for a in range(0,3):
     for b in range(0,3):
-      if puzzle[y0+a][x0+b] == n:
+      if puzzle[y2+a][x2+b] == n:
         return False
   return True
 
-def solve():
+def solvepuzzle():
   global puzzle
+  #check every square for 0 (blank space)
   for y in range(9):
     for x in range(9):
       if puzzle[y][x] == 0:
         for n in range (1,10):
           if reviewpossibles(y,x,n):
             puzzle[y][x] = n
-            solve()
+            solvepuzzle()
             puzzle[y][x] = 0
         return
   print(np.matrix(puzzle))
 
-solve()
+print("Welcome to my Sudoku solving application using Python.")
+print("When entering the current puzzle, separate each number with a comma (no spaces) and enter a 0 for each blank space.")
+user_input = input("Please enter the entire 1st row of the puzzle:")
+line0 = input_to_list(user_input)
+user_input = input("Please enter the entire 2nd row of the puzzle:")
+line1 = input_to_list(user_input)
+user_input = input("Please enter the entire 3rd row of the puzzle:")
+line2 = input_to_list(user_input)
+user_input = input("Please enter the entire 4th row of the puzzle:")
+line3 = input_to_list(user_input)
+user_input = input("Please enter the entire 5th row of the puzzle:")
+line4 = input_to_list(user_input)
+user_input = input("Please enter the entire 6th row of the puzzle:")
+line5 = input_to_list(user_input)
+user_input = input("Please enter the entire 7th row of the puzzle:")
+line6 = input_to_list(user_input)
+user_input = input("Please enter the entire 8th row of the puzzle:")
+line7 = input_to_list(user_input)
+user_input = input("Please enter the entire 9th row of the puzzle:")
+line8 = input_to_list(user_input)
+
+puzzle = [line0, line1, line2, line3, line4, line5, line6, line7, line8]
+
+solvepuzzle()
