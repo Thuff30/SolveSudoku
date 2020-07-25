faffe9017220f2b926a9f0d2e10debc7fe764fdd
Thuff30 <24842812+Thuff30@users.noreply.github.com>
2020-07-25 00:34:46 -0400

    Update SolveSudoku.py
    Added comments

--- SolveSudoku.py
+++ SolveSudoku.py
@@ -1,10 +1,12 @@
 import numpy as np
 
+#function to convert user input to a list of integers
 def input_to_list(input):
   listin = input.split(",")
   listout = list(map(int, listin))
   return listout
 
+#function to solve each square
 def reviewpossibles(y,x,n):
   global puzzle
   #check column
@@ -24,6 +26,7 @@ def reviewpossibles(y,x,n):
         return False
   return True
 
+#function to solve puzzle
 def solvepuzzle():
   global puzzle
   #check every square for 0 (blank space)
@@ -32,12 +35,17 @@ def solvepuzzle():
       if puzzle[y][x] == 0:
         for n in range (1,10):
           if reviewpossibles(y,x,n):
+            #enter solution from reviewpossible fucntion
             puzzle[y][x] = n
+            #use recursion to recall function until solved
             solvepuzzle()
+            #backtracking to reset incorrect answers to 0
             puzzle[y][x] = 0
         return
+  #print using numpy for readability
   print(np.matrix(puzzle))
 
+#gather user input
 print("Welcome to a simple Sudoku solving application using Python.")
 print("When entering the current puzzle, separate each number with a comma (no spaces) and enter a 0 for each blank space.")
 user_input = input("Please enter the entire 1st row of the puzzle:")
@@ -59,6 +67,7 @@ line7 = input_to_list(user_input)
 user_input = input("Please enter the entire 9th row of the puzzle:")
 line8 = input_to_list(user_input)
 
+#assign user input to list representing the Sudoku puzzle
 puzzle = [line0, line1, line2, line3, line4, line5, line6, line7, line8]
 
 solvepuzzle()
